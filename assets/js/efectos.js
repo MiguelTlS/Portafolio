b2541f1290a1ccdc493596db6b76ee6f40a0fc9e
Miguel <ts.miguel.a@gmail.com>
2023-01-18 11:46:01 -0600

    Version 0.8

---
+++ assets/js/efectos.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+    sr.reveal('.banner', {duration: 500, origin: 'bottom', distance: '-100px'});
+
+window.sr = ScrollReveal();
+    sr.reveal('.main-name', { duration: 2500, origin: 'right', distance: '-100px'});
+
+window.sr = ScrollReveal();
+    sr.reveal('.mb-item1', { duration: 3000, origin: 'right', distance: '200px'});
+
+window.sr = ScrollReveal();
+    sr.reveal('.project-skills-container', { duration: 1000, origin: 'top', distance: '400px'});
+
+/*window.sr = ScrollReveal();
+    sr.reveal('.pe-nested2', { duration: 3000, origin: 'left', distance: '350px'});
+window.sr = ScrollReveal();
+    sr.reveal('.pe-nested3', { duration: 3000, origin: 'left', distance: '450px'});
+window.sr = ScrollReveal();
+    sr.reveal('.pe-nested4', { duration: 3000, origin: 'left', distance: '500px'});*/
+window.sr = ScrollReveal();
+    sr.reveal('.pe-nested5', { duration: 3000, origin: 'right', distance: '350px'});
+window.sr = ScrollReveal();
+    sr.reveal('.pe-nested6', { duration: 3000, origin: 'right', distance: '450px'});
+window.sr = ScrollReveal();
+    sr.reveal('.pe-nested7', { duration: 3000, origin: 'right', distance: '500px'});
+
+window.sr = ScrollReveal();
+    sr.reveal('.s2-p', { duration: 2000, origin: 'left', distance: '500px'});
